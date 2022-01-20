3778addb49926ae47ce0426afff754e53cd625b9
Enviy <8320367+Enviy@users.noreply.github.com>
2022-01-19 20:46:40 -0800

    add highest resolution selection, progress bar

--- youtube_client.py
+++ youtube_client.py
@@ -1,13 +1,19 @@
 import argparse
 from pytube import YouTube
+from pytube.cli import on_progress
 
-
+# get_video collect highest resolution of url.
 def get_video(url):
     url = "".join(url)
     print("[*] Downloading video...\n")
-    YouTube(url).streams[0].download()
-
-
+    yt = YouTube(url, on_progress_callback=on_progress)
+    strem = yt.streams.get_highest_resolution()
+    if strem is not None:
+        strem.download()
+    else:
+        print("[!] No highest resolution found; None type returned.")
+  
+  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Interact with YouTube.")
     parser.add_argument("-g", "--get", nargs="*", help="Get a video.")
